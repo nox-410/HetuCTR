86ddb473348165e45d01c15e8dbc1b016857d579
Shi Yining <1700012728@pku.edu.cn>
2021-05-20 15:56:53 +0800

    Add zeromq to cmake

--- csrc/pybind/pybind.cc
+++ csrc/pybind/pybind.cc
@@ -1,6 +1,6 @@
 #include "pybind.h"
 
-PYBIND11_MODULE(hetu_gpu_cache, m) {
-  m.doc() = "hetu GPU cache C++/CUDA Implementation"; // module docstring
+PYBIND11_MODULE(hetu_gpu_table, m) {
+  m.doc() = "hetu GPU table C++/CUDA Implementation"; // module docstring
 
 } // PYBIND11_MODULE
